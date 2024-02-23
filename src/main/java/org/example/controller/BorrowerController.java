9c53f656c64994b2b43d90fe6b9fa3213ba2d662
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-02-24 02:44:27 +0530

    create post API,delete API

--- src/main/java/org/example/controller/BorrowerController.java
+++ src/main/java/org/example/controller/BorrowerController.java
@@ -2,9 +2,13 @@ package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.dto.Borrower;
+import org.example.entity.BorrowerEntity;
 import org.example.service.BorrowerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Iterator;
+import java.util.List;
 
 @RestController
 @RequestMapping("/borrower")
@@ -18,6 +22,16 @@ public class BorrowerController {
         service.addBorrower(borrower);
 
     }
+    @GetMapping("/get")
+    public Iterable<BorrowerEntity> getBorrower(){
+        return service.getBorrower();
+    }
+    @DeleteMapping("/remove/{id}")
+    public String deleteBorrower(@PathVariable Long id){
+        service.deleteBorrower(id);
+        return "delete borrower!";
+    }
+
 
 
 }
