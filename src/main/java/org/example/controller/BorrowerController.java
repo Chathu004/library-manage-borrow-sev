419405ca99ee2854742050a3a8450f41c18f99ad
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-02-24 00:42:45 +0530

    create get API

--- src/main/java/org/example/controller/BorrowerController.java
+++ src/main/java/org/example/controller/BorrowerController.java
@@ -1,4 +1,23 @@
 package org.example.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.dto.Borrower;
+import org.example.service.BorrowerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/borrower")
+@RequiredArgsConstructor
+@CrossOrigin
 public class BorrowerController {
+    @Autowired
+    BorrowerService service;
+    @PostMapping("/add")
+    public void addBorrower(@RequestBody Borrower borrower){
+        service.addBorrower(borrower);
+
+    }
+
+
 }
